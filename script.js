158171e65f76581fda72b88a7e5a4f5aa7293e84
Haridha M <smhharidha@gmail.com>
2024-11-22 15:42:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Navigation behavior
+document.getElementById('homeLink').addEventListener('click', function () {
+    window.location.href = '#landing';
+  });
+  
+  document.getElementById('articlesLink').addEventListener('click', function () {
+    window.location.href = '#articles';
+  });
+  
+  document.getElementById('galleryLink').addEventListener('click', function () {
+    window.location.href = '#gallery';
+  });
+  
+  document.getElementById('contactLink').addEventListener('click', function () {
+    window.location.href = '#contact';
+  });
+  
